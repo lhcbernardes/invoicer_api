7ae8c7a65c9867e734abfd2b5abb3942a340a740
Leandro <leandro.bernardes@hotmail.com>
2023-09-24 00:17:58 -0300

    Teste deploy

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const userRouter = require('./src/routes/users');
 const clientRouter = require('./src/routes/clients');
 const serviceRouter = require('./src/routes/services');
 const supportRouter = require('./src/routes/supports');
+const apiRouter = require('./src/api');
 
 const db = require('./src/data/database')
 db.connect()
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/', apiRouter)
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/clients', clientRouter);
